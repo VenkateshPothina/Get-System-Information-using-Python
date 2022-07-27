22d34353034bddc4c1f019fd9a512a256816b4f6
Venkatesh Pothina <venkateshpothina123@gmail.com>
2022-07-27 22:31:19 +0530

    Add files via upload

---
+++ systemhardwareinfo.py
@@ -0,0 +1,22 @@
+import platform
+
+# computer network name
+print(f"Computer network name: {platform.node()}")
+
+# machine name
+print(f"Machine type: {platform.machine()}")
+
+# processor type
+print(f"processor type: {platform.processor()}")
+
+# operating system 
+print(f"Operatng system type: {platform.system()}")
+
+#operating system release
+print(f"Operating system release: {platform.release()}")
+
+#operating system version
+print(f"operating system version: {platform.version()}")
+
+#python version
+print(f"python version: {platform.python_version()}")
